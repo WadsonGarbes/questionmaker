f62e739e1b57a3a8dcef58867be636682b624353
Wadson Garbes <nenogarbes@hotmail.com>
2024-10-10 16:40:49 -0300

    commit inicial

---
+++ questionmaker/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from questionmaker.users.models import User
+from questionmaker.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
